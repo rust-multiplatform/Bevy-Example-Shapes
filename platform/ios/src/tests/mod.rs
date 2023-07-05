fe656c40266902043f88b1178a4cd9798af4291d
Lukas Weber <dev@sakul-flee.de>
2023-07-05 20:16:27 +0200

    Disabled all tests (for now)

--- platform/ios/src/tests/mod.rs
+++ platform/ios/src/tests/mod.rs
@@ -1,13 +1,13 @@
-use crate::main_rs;
+// use crate::main_rs;
 
-#[test]
-fn check_main_rs_exists() {
-    main_rs();
-}
-
-// Example for UI tests:
 // #[test]
-// #[cfg(feature = "ui-tests")]
-// fn some_ui_test() {
-//     assert!(true);
+// fn check_main_rs_exists() {
+//     main_rs();
 // }
+
+// // Example for UI tests:
+// // #[test]
+// // #[cfg(feature = "ui-tests")]
+// // fn some_ui_test() {
+// //     assert!(true);
+// // }
